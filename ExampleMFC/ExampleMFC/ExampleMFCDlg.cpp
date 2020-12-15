d0568c4b05ac74c2d7ba9f30bf4111fc6646641d
ljkoy26-sudo <61034536+ljkoy26-sudo@users.noreply.github.com>
2020-12-15 15:50:25 +0900

    A,B,C,D 소멸자 부분 코드 제거
    1. A,B,C,D 소멸자 부분 코드 제거
     -> WVBase에서 이미 객체소멸이 이루어 지기에, 추가 작업 필요없기때문에 제거

--- ExampleMFC/ExampleMFC/ExampleMFCDlg.cpp
+++ ExampleMFC/ExampleMFC/ExampleMFCDlg.cpp
@@ -178,14 +178,12 @@ void CExampleMFCDlg::OnBnClickedButton1() // Carray테스트
 	WVChildA* pa = new WVChildA; // 리스너
 	WVChildB* pb = new WVChildB; // 디스크립션 리스트
 	WVChildC* pc = new WVChildC; // 디스크립션
-	WVChildC* pc1 = new WVChildC; // 디스크립션
-	WVChildC* pc2 = new WVChildC; // 디스크립션
+	//WVChildC* pc1 = new WVChildC; // 디스크립션
+	//WVChildC* pc2 = new WVChildC; // 디스크립션
 	WVChildD* pd = new WVChildD; // 
 	
 	pc->m_aItem.Add(pd);
-	pa->m_aItem.Add(pc1); // 리스트는 없는 상태
 	pa->m_aItem.Add(pc); // 리스트는 없는 상태
-	pa->m_aItem.Add(pc2); // 리스트는 없는 상태
 	
 	for (int i = 0; pa->m_aItem.GetSize(); ++i)
 	{
@@ -193,6 +191,7 @@ void CExampleMFCDlg::OnBnClickedButton1() // Carray테스트
 		{
 			/* 한개의 케이스 에서는 성공했지만, 
 				pc 가 여러개 있을경우나, 순서가 뒤죽박죽이면 조금 결과가 달라짐...
+				노트북에서 출력 테스트 완료 20.12.15
 			*/
 			WVBase* pItem = pa->m_aItem.ElementAt(i);
 			pa->m_aItem.RemoveAt(i);
--- ExampleMFC/ExampleMFC/WVChildA.cpp
+++ ExampleMFC/ExampleMFC/WVChildA.cpp
@@ -10,27 +10,4 @@ WVChildA::WVChildA()
 
 WVChildA::~WVChildA()
 {
-	int nSize(m_aItem.GetSize());
-	CString strtype;
-	CString str;
-
-	for (int i = 0; i < nSize; ++i)
-	{
-		WVBase* pItem = m_aItem.GetAt(i);
-
-		switch (pItem->m_nType)
-		{
-		case 0:			strtype = _T("BASE");		break;
-		case 1:			strtype = _T("A	");		break;
-		case 2:			strtype = _T("B	");		break;
-		case 3:			strtype = _T("C	");		break;
-		case 4:			strtype = _T("D	");		break;
-		default:		strtype.Format(_T("%d"), pItem->m_nType); break;
-		}
-
-		str.Format(_T("pItemÅ¸ÀÔ ==> (%s)\n"), strtype);
-		TRACE(str);
-		delete pItem;
-	}
-	m_aItem.RemoveAll();
 }
